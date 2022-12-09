74eb20259b7b4baa1c78878238134cbfb877b170
jp_geronimo <jgeronimo@vertere-gs.com>
2022-12-09 17:13:25 +0800

    Fixed dynamic updating in reviews page

--- src/app/pages/add-review/add-review.component.ts
+++ src/app/pages/add-review/add-review.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 import { Review } from '@models/review';
 import { ReviewService } from '@services/review.service';
 import Swal from 'sweetalert2';
@@ -11,19 +12,40 @@ import Swal from 'sweetalert2';
 export class AddReviewComponent implements OnInit, OnDestroy {
 
   review: Review = new Review();
+  stars: Array<number> = new Array(5);
+  starValue: number = 1;
   @Input() courseId!: number;
   @Input() userId!: string;
 
+  @Output() refreshReviews = new EventEmitter<string>();
+
   constructor(
-    private reviewService: ReviewService) 
+    private reviewService: ReviewService,
+    private router: Router) 
     {  }
 
   ngOnInit(): void {
   }
 
+  refreshToggle() {
+    this.refreshReviews.emit();
+  }
+
+  setRating(rating: number): void {
+    console.log(rating);
+    this.starValue = rating;
+    this.review.rating = this.starValue;
+  }
+
+
+
   onSubmit(): void {
+     
+      this.review.rating = this.starValue;
+
       this.reviewService.save(this.courseId, this.userId, this.review).subscribe((response) => {
         console.log(response);
+        this.refreshToggle();
       })
   }
 
